496b2703914b17ddf0d5320e24c3bc273d4800aa
Juan Felipe <103801874+JuanLadeira@users.noreply.github.com>
2023-02-21 20:42:43 -0300

    jogo da memória finalizado

--- memoria.js
+++ memoria.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 const cardArray = [
     {
         name: 'fries',
@@ -5,7 +6,7 @@ const cardArray = [
     },
     {
         name: 'cheeseburguer',
-        img: 'images/cheeseburguer.png'
+        img: 'images/cheeseburger.png'
     },
     {
         name: 'milkshake',
@@ -29,7 +30,7 @@ const cardArray = [
     },
     {
         name: 'cheeseburguer',
-        img: 'images/cheeseburguer.png'
+        img: 'images/cheeseburger.png'
     },
     {
         name: 'milkshake',
@@ -52,15 +53,66 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay  = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+const pontuacaoDisplay = document.querySelector('#pontuacao')
+let cardsChosen = []
+let cardsChosenIds = []
+let cardsWon = []
+
+
 
 function createBoard(){
     for (let i = 0;i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        console.log(card)
+        card.addEventListener('click', flipcard)
         gridDisplay.append(card)   
     }
 }
 
+function flipcard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch(){
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    if(optionOneId == optionTwoId){
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert("Você clicou duas vezes na mesma figura, tente novamente")
+    }
+    else if(cardsChosen[0] == cardsChosen[1]){
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipcard)
+        cards[optionTwoId].removeEventListener('click', flipcard)
+        alert('Você achou um Match!!')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert("Você não conseguiu, tente novamente!!")
+    }
+    
+    cardsChosen = []
+    cardsChosenIds = []
+    console.log(cardsWon.length)
+    resultDisplay.textContent = cardsWon.length + " pontos"
+
+    if(cardsWon.length==cardArray.length/2){
+        pontuacaoDisplay.textContent = ""
+        resultDisplay.innerHTML = 'Parabéns, você encontrou todas as duplicatas.'
+    }
+}
 createBoard()
+
+})
