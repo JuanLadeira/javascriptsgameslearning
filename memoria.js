3dff85da9298c967733e872d56d993d09f3588ad
Juan Felipe <103801874+JuanLadeira@users.noreply.github.com>
2023-02-21 18:50:18 -0300

    adicionando as imagens e inserindo elementos no grid pelo js

--- memoria.js
+++ memoria.js
@@ -1,3 +1,66 @@
 const cardArray = [
-    
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburguer',
+        img: 'images/cheeseburguer.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburguer',
+        img: 'images/cheeseburguer.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay  = document.querySelector('#grid')
+
+function createBoard(){
+    for (let i = 0;i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        console.log(card)
+        gridDisplay.append(card)   
+    }
+}
+
+createBoard()
